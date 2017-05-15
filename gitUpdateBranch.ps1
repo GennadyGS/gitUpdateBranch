f0a0a96a700823ee8722cbcbf0f9fa9910d8b107
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-05-15 11:00:32 +0300

    Improve error handling

---
+++ gitUpdateBranch.ps1
@@ -0,0 +1,61 @@
+﻿param(
+    $remoteName = 'origin',
+    $sourceBranchName = 'master'
+)
+
+$outputPath = "$PSScriptRoot\Output"
+$timeSuffix = Get-Date -format yyyyMMddHHmmss
+$logFileName = "$outputPath\gitUpdateBranch$timeSuffix.log"
+
+Function RunGit {
+    param (
+        $gitArgsStr,
+        [switch]$noCheck
+    )
+
+    Write-Host "git $gitArgsStr" -ForegroundColor yellow
+
+    Invoke-Expression "git $gitArgsStr"
+    if ((!$noCheck) -and  ($LastExitCode -ne 0)) {
+        throw "'git $gitArgsStr' returned code $LastExitCode"
+    }
+}
+
+if (!(Test-Path -Path $outputPath))
+{
+    New-Item $outputPath -type directory | Out-Null
+}
+
+try {
+    Write-Output "Starting transcription to $logFileName..."
+    Start-Transcript -path $logFileName
+}
+catch {
+    Write-Error "Error starting transcription: $_";
+}
+
+Write-Output "Current directory is $PWD"
+try {
+    try {
+        RunGit "stash"
+        RunGit "pull -p"
+        RunGit "merge $remoteName/$sourceBranchName"
+        RunGit "push"
+        RunGit -noCheck "stash pop"
+    }
+    catch {
+        Write-Error -ErrorRecord $Error[0]
+        throw
+    }
+    finally {
+        try{
+            Stop-Transcript
+        }
+        catch {
+            Write-Error "Error stopping transcription: $_";
+        }
+    }
+}
+catch {
+    # TODO: Send email
+}
